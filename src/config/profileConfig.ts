554668b350dadac93a29b0e74f323daeea7531e6
Lennethhh <1915694404@qq.com>
2026-09-26 01:17:11 +0800

    个人资料2修

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -22,6 +22,12 @@ export const profileConfig: ProfileConfig = {
 	// showName: true 时显示图标和名称，false 时只显示图标
 	links: [
 		{
+			name: "QQ群",
+			icon: "fa7-brands:qq",
+			url: "",
+			showName: false,
+		},
+		{
 			name: "Bilibili",
 			icon: "fa7-brands:bilibili",
 			url: "https://www.bilibili.com/",
@@ -31,12 +37,6 @@ export const profileConfig: ProfileConfig = {
 			name: "GitHub",
 			icon: "fa7-brands:github",
 			url: "https://github.com/Lennethhh",
-			showName: false,
-		},
-		{
-			name: "QQ群",
-			icon: "fa7-brands:qq",
-			url: "",
 			showName: false,
 		},
 		{
